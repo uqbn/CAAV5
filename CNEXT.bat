9046037c65070d10d7b50c2c3cdaa53fbc8f8b41
uqbn <uqbn@foxmail.com>
2024-12-22 19:47:00 +0800

    update project

--- CNEXT.bat
+++ CNEXT.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "%~dp0/.."
+cd /d %~dp0..
 set CNEXTOUTPUT=console
 for %%i in ("%cd%") do set "CATDefaultEnvironment=Workspace_%%~ni"
 "C:/Program Files/Dassault Systemes/B22/win_b64/code/bin/CATStart.exe" -nowindow -run CNEXT.exe
--- clean.bat
+++ clean.bat
@@ -1,6 +1,6 @@
 @echo off
 title clean
-call C:\PROGRA~2\RADE22\intel_a\code\command\tck_init.bat >NUL
-call tck_profile V5_6R2012_B22 >NUL
+call C:\PROGRA~2\RADE22\intel_a\code\command\tck_init.bat >nul
+call tck_profile V5_6R2012_B22 >nul
 call mkRemoveDo -a
 pause
